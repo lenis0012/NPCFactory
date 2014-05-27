29a13b3ef3beceacd78ace6df5df453fb3929ced
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-05-27 18:08:07 +0200

    Fixed lookAt function

--- src/main/java/com/lenis0012/bukkit/npc/NPC.java
+++ src/main/java/com/lenis0012/bukkit/npc/NPC.java
@@ -71,7 +71,9 @@ public class NPC {
 	private final float getAngle(Vector point1, Vector point2) {
 		double dx = point2.getX() - point1.getX();
 		double dz = point2.getZ() - point1.getZ();
-		return (float) Math.toDegrees(Math.atan2(dz, dx));
+		float angle = (float) Math.toDegrees(Math.atan2(dz, dx)) - 90;
+		if(angle < 0) { angle += 360.0F; }
+		return angle;
 	}
 	
 	/**
