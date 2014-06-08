80a2472239405d6f8663d54ec69b0e5b23eec6da
DoctorDark <DoctorDark@users.noreply.github.com>
2014-06-08 13:40:37 +0100

    Added setArmour and setHandItem methods.

--- src/main/java/com/lenis0012/bukkit/npc/NPC.java
+++ src/main/java/com/lenis0012/bukkit/npc/NPC.java
@@ -1,8 +1,9 @@
-package com.lenis0012.bukkit.npc;
+package hardcore.npc.lib;
 
 import org.bukkit.Location;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Class that contains all api functions from npc's in NPCFactory
@@ -10,101 +11,144 @@ import org.bukkit.entity.Player;
  * @author lenis0012
  */
 public interface NPC {
-	
+
 	/**
-	 * Get an org.bukkit.entity.Player instance from the npc.
+	 * Get an org.bukkit.entity.Player instance from the BOC.
 	 * 
 	 * @return NPC's player instance.
 	 */
 	public Player getBukkitEntity();
-	
+
 	/**
-	 * Check wether or not an npc can get damaged
+	 * Check whether or not an npc can get damaged
 	 * 
 	 * @return NPC can be damaged?
 	 */
 	public boolean isInvulnerable();
-	
+
 	/**
-	 * Set wether or not an npc can get damaged
+	 * Set whether or not an npc can get damaged
 	 * 
 	 * @param invulnerable NPC can be damaged?
 	 */
 	public void setInvulnerable(boolean invulnerable);
-	
+
 	/**
-	 * Check wether or not an npc has gravity enabled.
+	 * Check whether or not an npc has gravity enabled.
 	 * 
-	 * @return NPC had grvity?
+	 * @return NPC had gravity?
 	 */
 	public boolean isGravity();
-	
+
 	/**
-	 * Set wether or not an npc has gravity enabled.
+	 * Set whether or not an npc has gravity enabled.
 	 * 
 	 * @param gravity NPC has gravity?
 	 */
 	public void setGravity(boolean gravity);
 	
 	/**
-	 * Walk to a location
+	 * Set an NPC's armour contents.
 	 * 
-	 * @param location Location to walk to
+	 * @param Set an npc's armour contents.
+	 */
+	public boolean setArmorContents(ItemStack[] armor);
+	
+	/**
+	 * Set an NPC's held item.
+	 * 
+	 * @param Set an npc's held item.
+	 */
+	public boolean setHeldItem(ItemStack hand);
+	
+	/**
+	 * Set an NPC's helmet.
+	 * 
+	 * @param Set an npc's helmet.
+	 */
+	public boolean setHelmet(ItemStack helmet);
+	
+	/**
+	 * Set an NPC's chestplate.
+	 * 
+	 * @param Set an npc's chestplate.
+	 */
+	public boolean setChestplate(ItemStack chestplate);
+	
+	/**
+	 * Set an NPC's leggings.
+	 * 
+	 * @param Set an npc's leggings.
+	 */
+	public boolean setLeggings(ItemStack leggings);
+	
+	/**
+	 * Set an NPC's boots.
+	 * 
+	 * @param Set an npc's boots.
+	 */
+	public boolean setBoots(ItemStack boots);
+
+	/**
+	 * Walk to a location.
+	 * 
+	 * @param location Location to walk to.
 	 * @return True if path created and applied correctly.
 	 */
 	public boolean pathfindTo(Location location);
-	
+
 	/**
-	 * Walk to a location with custom speed
+	 * Walk to a location with custom speed.
 	 * 
-	 * @param location Location to walk to
-	 * @param speed Speed to walk with (max 1.0, 0.2 default)
+	 * @param location Location to walk to.
+	 * @param speed Speed to walk with (max 1.0, 0.2 default).
 	 * @return True if path created and applied correctly.
 	 */
 	public boolean pathfindTo(Location location, double speed);
-	
+
 	/**
 	 * Walk to a location with custom speed and range.
 	 * 
-	 * @param location Location to walk to
-	 * @param speed Speed to walk with (max 1.0, 0.2 default)
-	 * @param range Block radius limit for path finding (30 default)
+	 * @param location Location to walk to.
+	 * @param speed Speed to walk with (max 1.0, 0.2 default).
+	 * @param range Block radius limit for path finding (30 default).
 	 * @return True if path created and applied correctly.
 	 */
 	public boolean pathfindTo(Location location, double speed, double range);
-	
+
 	/**
-	 * Make NPC look at an entity.
+	 * Make NPC look at an entity..
 	 * 
-	 * @param target Entity to look at
+	 * @param entity Entity to look at.
 	 */
-	public void setTarget(LivingEntity target);
-	
+	public void setTarget(Entity entity);
+
 	/**
-	 * Get the entity the ncp is looking at
+	 * Get the entity the NPC is looking at.
 	 * 
-	 * @return Entity npc is looking at (null if not found)
+	 * @return Entity NPC is looking at (null if not found).
 	 */
-	public LivingEntity getTarget();
-	
+	public Entity getTarget();
+
 	/**
-	 * Make npc look at a certain location
+	 * Make NPC look at a certain location.
 	 * 
-	 * @param location Location to look at
+	 * @param location Location to look at.
 	 */
 	public void lookAt(Location location);
-	
+
 	/**
-	 * Change npc's yaw the proper way
+	 * Change NPC's yaw the proper way.
 	 * 
-	 * @param yaw New npc yaw
+	 * @param yaw New NPC yaw.
 	 */
 	public void setYaw(float yaw);
-	
+
 	/**
-	 * Play an animcation on the npc
+	 * Play an animation on the NPC.
 	 * 
-	 * @param animcation Animcation type to display
+	 * @param animation Animcation type to display.
 	 */
 	public void playAnimation(NPCAnimation animcation);
+	
 }
