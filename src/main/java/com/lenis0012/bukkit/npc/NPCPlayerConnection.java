ad9c623640c468fe49a0d43d98c8f35d7fc05f72
stopbox <stopbox@users.noreply.github.com>
2014-09-24 14:53:03 -0500

    Fix  crashing issues
    Since it try's using the normal methods you must replace it with a null method

--- src/main/java/com/lenis0012/bukkit/npc/NPCPlayerConnection.java
+++ src/main/java/com/lenis0012/bukkit/npc/NPCPlayerConnection.java
@@ -12,4 +12,103 @@ public class NPCPlayerConnection extends PlayerConnection {
 	public NPCPlayerConnection(NetworkManager networkmanager, EntityPlayer entityplayer) {
 		super(((CraftServer) Bukkit.getServer()).getServer(), networkmanager, entityplayer);
 	}
+	
+		@Override
+	public void disconnect(String s) {
+	}
+
+	@Override
+	public void a(PacketPlayInSteerVehicle packetplayinsteervehicle) {
+
+	}
+
+	@Override
+	public void a(PacketPlayInFlying packetplayinflying) {
+	}
+
+	@Override
+	public void a(PacketPlayInBlockDig packetplayinblockdig) {
+	}
+
+	@Override
+	public void a(PacketPlayInBlockPlace packetplayinblockplace) {
+	}
+
+	@Override
+	public void a(IChatBaseComponent ichatbasecomponent) {
+	}
+
+	@Override
+	public void sendPacket(Packet packet) {
+	}
+
+	@Override
+	public void a(PacketPlayInChat packetplayinchat) {
+	}
+
+	@Override
+	public void a(PacketPlayInArmAnimation packetplayinarmanimation) {
+	}
+
+	@Override
+	public void a(PacketPlayInEntityAction packetplayinentityaction) {
+	}
+
+	@Override
+	public void a(PacketPlayInUseEntity packetplayinuseentity) {
+	}
+
+	@Override
+	public void a(PacketPlayInClientCommand packetplayinclientcommand) {
+	}
+
+	@Override
+	public void a(PacketPlayInCloseWindow packetplayinclosewindow) {
+	}
+
+	@Override
+	public void a(PacketPlayInWindowClick packetplayinwindowclick) {
+	}
+
+	@Override
+	public void a(PacketPlayInEnchantItem packetplayinenchantitem) {
+	}
+
+	@Override
+	public void a(PacketPlayInSetCreativeSlot packetplayinsetcreativeslot) {
+	}
+
+	@Override
+	public void a(PacketPlayInTransaction packetplayintransaction) {
+	}
+
+	@Override
+	public void a(PacketPlayInUpdateSign packetplayinupdatesign) {
+	}
+
+	@Override
+	public void a(PacketPlayInKeepAlive packetplayinkeepalive) {
+
+	}
+
+	@Override
+	public void a(PacketPlayInAbilities packetplayinabilities) {
+
+	}
+
+	@Override
+	public void a(PacketPlayInTabComplete packetplayintabcomplete) {
+	}
+
+	@Override
+	public void a(PacketPlayInSettings packetplayinsettings) {
+	}
+
+	@Override
+	public void a(PacketPlayInCustomPayload packetplayincustompayload) {
+	}
+
+	@Override
+	public void a(EnumProtocol enumprotocol, EnumProtocol enumprotocol1) {
+	}
 }
